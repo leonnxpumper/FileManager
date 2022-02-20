da40a6380e58393d77796fbdd87b1c808c0dec8c
leonnxpumper <salvadorbenitez90@gmail.com>
2022-02-19 23:59:47 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_Back = new System.Windows.Forms.Button();
+            this.btn_Go = new System.Windows.Forms.Button();
+            this.txt_FilePath = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.iconList = new System.Windows.Forms.ImageList(this.components);
+            this.lbl_Name_FileName = new System.Windows.Forms.Label();
+            this.lbl_FileName = new System.Windows.Forms.Label();
+            this.lbl_name_FileType = new System.Windows.Forms.Label();
+            this.lbl_FileType = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.createFIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createNewDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SuspendLayout();
+            // 
+            // btn_Back
+            // 
+            this.btn_Back.Location = new System.Drawing.Point(12, 12);
+            this.btn_Back.Name = "btn_Back";
+            this.btn_Back.Size = new System.Drawing.Size(75, 23);
+            this.btn_Back.TabIndex = 0;
+            this.btn_Back.Text = "Back";
+            this.btn_Back.UseVisualStyleBackColor = true;
+            this.btn_Back.Click += new System.EventHandler(this.btn_Back_Click);
+            // 
+            // btn_Go
+            // 
+            this.btn_Go.Location = new System.Drawing.Point(713, 12);
+            this.btn_Go.Name = "btn_Go";
+            this.btn_Go.Size = new System.Drawing.Size(75, 23);
+            this.btn_Go.TabIndex = 1;
+            this.btn_Go.Text = "Go";
+            this.btn_Go.UseVisualStyleBackColor = true;
+            this.btn_Go.Click += new System.EventHandler(this.btn_Go_Click);
+            // 
+            // txt_FilePath
+            // 
+            this.txt_FilePath.Location = new System.Drawing.Point(93, 12);
+            this.txt_FilePath.Name = "txt_FilePath";
+            this.txt_FilePath.Size = new System.Drawing.Size(603, 20);
+            this.txt_FilePath.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.LabelEdit = true;
+            this.listView1.LargeImageList = this.iconList;
+            this.listView1.Location = new System.Drawing.Point(12, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 346);
+            this.listView1.SmallImageList = this.iconList;
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.AfterLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.listView1_AfterLabelEdit);
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView1_ItemSelectionChanged);
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // iconList
+            // 
+            this.iconList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("iconList.ImageStream")));
+            this.iconList.TransparentColor = System.Drawing.Color.Transparent;
+            this.iconList.Images.SetKeyName(0, "iDoc.png");
+            this.iconList.Images.SetKeyName(1, "iDocX.png");
+            this.iconList.Images.SetKeyName(2, "ijpeg.png");
+            this.iconList.Images.SetKeyName(3, "ijpg.png");
+            this.iconList.Images.SetKeyName(4, "ipdf.png");
+            this.iconList.Images.SetKeyName(5, "iUnknown.png");
+            this.iconList.Images.SetKeyName(6, "iFolder.png");
+            this.iconList.Images.SetKeyName(7, "iPng.png");
+            this.iconList.Images.SetKeyName(8, "itxt.png");
+            this.iconList.Images.SetKeyName(9, "izip.png");
+            this.iconList.Images.SetKeyName(10, "iexcel.png");
+            // 
+            // lbl_Name_FileName
+            // 
+            this.lbl_Name_FileName.AutoSize = true;
+            this.lbl_Name_FileName.Location = new System.Drawing.Point(22, 414);
+            this.lbl_Name_FileName.Name = "lbl_Name_FileName";
+            this.lbl_Name_FileName.Size = new System.Drawing.Size(54, 13);
+            this.lbl_Name_FileName.TabIndex = 4;
+            this.lbl_Name_FileName.Text = "File Name";
+            // 
+            // lbl_FileName
+            // 
+            this.lbl_FileName.AutoSize = true;
+            this.lbl_FileName.Location = new System.Drawing.Point(90, 414);
+            this.lbl_FileName.Name = "lbl_FileName";
+            this.lbl_FileName.Size = new System.Drawing.Size(0, 13);
+            this.lbl_FileName.TabIndex = 5;
+            // 
+            // lbl_name_FileType
+            // 
+            this.lbl_name_FileType.AutoSize = true;
+            this.lbl_name_FileType.Location = new System.Drawing.Point(637, 414);
+            this.lbl_name_FileType.Name = "lbl_name_FileType";
+            this.lbl_name_FileType.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbl_name_FileType.Size = new System.Drawing.Size(50, 13);
+            this.lbl_name_FileType.TabIndex = 6;
+            this.lbl_name_FileType.Text = "File Type";
+            // 
+            // lbl_FileType
+            // 
+            this.lbl_FileType.AutoSize = true;
+            this.lbl_FileType.Location = new System.Drawing.Point(739, 414);
+            this.lbl_FileType.Name = "lbl_FileType";
+            this.lbl_FileType.Size = new System.Drawing.Size(0, 13);
+            this.lbl_FileType.TabIndex = 7;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // createFIleToolStripMenuItem
+            // 
+            this.createFIleToolStripMenuItem.Name = "createFIleToolStripMenuItem";
+            this.createFIleToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.createFIleToolStripMenuItem.Text = "Create File";
+            // 
+            // createNewDirectoryToolStripMenuItem
+            // 
+            this.createNewDirectoryToolStripMenuItem.Name = "createNewDirectoryToolStripMenuItem";
+            this.createNewDirectoryToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.createNewDirectoryToolStripMenuItem.Text = "Create New Directory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_FileType);
+            this.Controls.Add(this.lbl_name_FileType);
+            this.Controls.Add(this.lbl_FileName);
+            this.Controls.Add(this.lbl_Name_FileName);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.txt_FilePath);
+            this.Controls.Add(this.btn_Go);
+            this.Controls.Add(this.btn_Back);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Back;
+        private System.Windows.Forms.Button btn_Go;
+        private System.Windows.Forms.TextBox txt_FilePath;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label lbl_Name_FileName;
+        private System.Windows.Forms.Label lbl_FileName;
+        private System.Windows.Forms.Label lbl_name_FileType;
+        private System.Windows.Forms.Label lbl_FileType;
+        private System.Windows.Forms.ImageList iconList;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem createFIleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createNewDirectoryToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = "D:/";
+        private bool isFile = false;
+        private string currentlySelectedItemName = "";
+        private bool copying  = false;
+        private bool moving = false;
+        private string pathFiletoCopyMove = "";
+        private string nameFiletoCopyMove = ""; 
+        public Form1()
+        {
+            InitializeComponent();
+            CreateHeadersAndFillListView();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_FilePath.Text = filePath;
+            loadFilesAndDirectory();
+        }
+
+
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            goBack();
+            loadBtn_Action();
+        }
+
+        private void btn_Go_Click(object sender, EventArgs e)
+        {
+            loadBtn_Action();
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            try
+            {
+                currentlySelectedItemName = e.Item.Text;
+                FileAttributes fileAttr = File.GetAttributes(filePath + "/" + currentlySelectedItemName);
+                if ((fileAttr & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    //It will show us the directory name on address bar
+                    isFile = false;
+                    txt_FilePath.Text = filePath + "/" + currentlySelectedItemName;
+                }
+                else
+                {
+                    isFile = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                isFile = false;
+            }
+            
+
+        }
+
+        public void removeBackSlash()
+        {
+            string path = txt_FilePath.Text;
+            if((path.LastIndexOf("/") == path.Length - 1) && path!="D:/")
+            {
+                txt_FilePath.Text=path.Substring(0, path.Length-1);
+            }
+        }
+
+        public void goBack()
+        {
+            try
+            {
+                removeBackSlash();
+                string path = txt_FilePath.Text;
+                path=path.Substring(0,path.LastIndexOf('/'));
+                this.isFile = false; //It will be a directory
+                txt_FilePath.Text=path;
+                removeBackSlash();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+
+        private void loadFilesAndDirectory()
+        {
+            DirectoryInfo fileList;
+            string tempFilePath = "";
+            FileAttributes fileAttr;
+            try
+            {
+                if (isFile)
+                {
+                    tempFilePath = filePath + "/" + currentlySelectedItemName;
+                    FileInfo fileInfo = new FileInfo(tempFilePath);
+                    lbl_FileName.Text = fileInfo.Name;
+                    lbl_FileType.Text = fileInfo.Extension;
+                    fileAttr = File.GetAttributes(tempFilePath);
+
+                    Process.Start(tempFilePath); //You can Open Files with this
+                }
+                else
+                {
+
+                    fileAttr = File.GetAttributes(filePath);
+
+                }
+
+                if ((fileAttr & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    fileList = new DirectoryInfo(filePath);
+                    FileInfo[] files = fileList.GetFiles(); // With this will get all files from the directory
+                    DirectoryInfo[] dirs = fileList.GetDirectories();// With this will get additional directories from the directory
+                    string fileExtension = "";
+                    listView1.Items.Clear();
+                    //Adding Files
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        ListViewItem lvi = new ListViewItem();
+                        lvi.Text = files[i].Name;
+                        
+                        fileExtension= files[i].Extension.ToLower();
+                        switch (fileExtension)
+                        {
+                            case ".doc":
+                            case ".docx":
+                                lvi.ImageIndex = 0;
+                                //listView1.Items.Add(files[i].Name,0);
+                                break;
+                            case ".jpeg":
+                            case ".jpg":
+                                lvi.ImageIndex = 2;
+                                //listView1.Items.Add(files[i].Name, 2);
+                                break;
+                            case ".png":
+                                lvi.ImageIndex = 2;
+                                //listView1.Items.Add(files[i].Name, 2);
+                                break ;
+                            case ".pdf":
+                                lvi.ImageIndex = 4;
+                                //listView1.Items.Add(files[i].Name, 4);
+                                break;
+                            case ".txt":
+                                lvi.ImageIndex = 8;
+                                //listView1.Items.Add(files[i].Name, 4);
+                                break;
+                            case ".zip":
+                                lvi.ImageIndex = 9;
+                                //listView1.Items.Add(files[i].Name, 4);
+                                break;
+                            case ".xls":
+                            case ".xlsx":
+                                lvi.ImageIndex = 10;
+                                //listView1.Items.Add(files[i].Name, 4);
+                                break;
+                            default:
+                                lvi.ImageIndex = 5;
+                                //listView1.Items.Add(files[i].Name, 5);
+                                break;
+                        }
+                        lvi.SubItems.Add( new ListViewItem.ListViewSubItem().Text=files[i].Extension);
+                        lvi.SubItems.Add(new ListViewItem.ListViewSubItem().Text = files[i].Length.ToString());
+                        lvi.SubItems.Add(new ListViewItem.ListViewSubItem().Text = files[i].LastWriteTime.ToString());
+                        lvi.SubItems.Add(new ListViewItem.ListViewSubItem().Text = files[i].Attributes.ToString());
+                        listView1.Items.Add(lvi);
+                    }
+
+                    //Adding Directories
+                    for (int i = 0; i < dirs.Length; i++)
+                    {
+                        ListViewItem lvi = new ListViewItem();
+                        lvi.Text = dirs[i].Name;
+                        lvi.ImageIndex = 6;
+
+                        lvi.SubItems.Add(new ListViewItem.ListViewSubItem().Text = dirs[i].Extension);
+                        lvi.SubItems.Add(new ListViewItem.ListViewSubItem().Text = "<DIR>");
+                        lvi.SubItems.Add(new ListViewItem.ListViewSubItem().Text = dirs[i].LastWriteTime.ToString());
+                        lvi.SubItems.Add(new ListViewItem.ListViewSubItem().Text = dirs[i].Attributes.ToString());
+                        listView1.Items.Add(lvi);
+                    }
+                }
+                else
+                {
+                    lbl_FileName.Text = this.currentlySelectedItemName;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+            
+
+        }
+        private void loadBtn_Action()
+        {
+            removeBackSlash();
+            filePath = txt_FilePath.Text;
+            loadFilesAndDirectory();
+            isFile = false;
+
+
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            loadBtn_Action();
+        }
+
+        private void CreateHeadersAndFillListView()
+        {
+            ColumnHeader colHead;
+
+            colHead = new ColumnHeader();
+            colHead.Width = 250;
+            colHead.Text = "Filename";
+            this.listView1.Columns.Add(colHead);
+
+            colHead = new ColumnHeader();
+            colHead.Width = 50;
+            colHead.Text = "Ext";
+            this.listView1.Columns.Add(colHead);
+
+            colHead = new ColumnHeader();
+            colHead.Width = 100;
+            colHead.Text = "Size";
+            this.listView1.Columns.Add(colHead);
+
+            colHead = new ColumnHeader();
+            colHead.Width = 200;
+            colHead.Text = "Date";
+            this.listView1.Columns.Add(colHead);
+
+            colHead = new ColumnHeader();
+            colHead.Width = 176;
+            colHead.Text = "Attr";
+            this.listView1.Columns.Add(colHead);
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            { 
+                var focusedItem = listView1.FocusedItem;
+                ContextMenu cm = new ContextMenu();
+                listView1.ContextMenu = cm;
+                if (focusedItem.Bounds.Contains(e.Location))
+                {
+                        if (focusedItem != null)
+                        {
+                            cm.MenuItems.Add(new MenuItem("Rename", OnMenuItemClick));
+                            cm.MenuItems.Add(new MenuItem("Delete", OnMenuDelete));
+                            cm.MenuItems.Add(new MenuItem("Copy", OnMenuCopy));
+                            cm.MenuItems.Add(new MenuItem("Move", OnMenuMove));
+                            cm.MenuItems.Add(new MenuItem("Paste", OnMenuPaste));
+                        }    
+                }
+                
+            }
+        }
+
+        //RENAME
+
+        private void OnMenuItemClick(object sender, EventArgs e)
+        {
+            listView1.SelectedItems[0].BeginEdit();
+        }
+        private void OnMenuDelete(object sender, EventArgs e)
+        {
+            ListViewItem item = listView1.SelectedItems[0];
+            FileInfo fileinfo = new FileInfo(filePath + "/" + currentlySelectedItemName);
+            File.Delete(fileinfo.FullName);
+            currentlySelectedItemName = "";
+            listView1.SelectedItems[0].Remove();
+        }
+
+        private void OnMenuCopy(object sender, EventArgs e)
+        {
+            ListViewItem item = listView1.SelectedItems[0];
+            FileInfo fileinfo = new FileInfo(filePath + "/" + currentlySelectedItemName);
+            copying = true;
+            pathFiletoCopyMove = fileinfo.FullName;
+            nameFiletoCopyMove = fileinfo.Name;
+        }
+        private void OnMenuMove(object sender, EventArgs e)
+        {
+            ListViewItem item = listView1.SelectedItems[0];
+            FileInfo fileinfo = new FileInfo(filePath + "/" + currentlySelectedItemName);
+            moving = true;
+            pathFiletoCopyMove = fileinfo.FullName;
+            nameFiletoCopyMove = fileinfo.Name;
+        }
+        private void OnMenuPaste(object sender, EventArgs e)
+        {
+            if (copying)
+            {
+                File.Copy(pathFiletoCopyMove, filePath + "/" + nameFiletoCopyMove);
+            }
+            if (moving)
+            {
+                File.Move(pathFiletoCopyMove, filePath + "/" + nameFiletoCopyMove);
+            }
+            copying = false;
+            moving = false;
+        }
+
+        private void onNewtxtFile(object sender, EventArgs e)
+        {
+            listView1.CreateControl();
+        }
+
+        private void listView1_AfterLabelEdit(object sender, LabelEditEventArgs e)
+        {
+            if (e.Label == null)
+                return;
+            string newName = Convert.ToString(e.Label);
+            ListViewItem item = listView1.SelectedItems[0];
+            FileInfo fileinfo = new FileInfo(filePath + "/" + currentlySelectedItemName);
+
+            fileinfo.MoveTo(fileinfo.Directory.FullName + "\\" + newName);
+            currentlySelectedItemName=newName;
+            listView1.Items[0].Text = newName;
+            
+        }
+
+
+
+
+
+
+
+        //DELETE
+    }
+}
